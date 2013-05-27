aaed62ab5a3dc817c39b791b7b42e840420d2076
harakachi <harakachi@gmail.com>
2013-05-27 18:51:38 +0900

    Add initial files
    my first commit

--- jankenGame/MixiViewController.h
+++ jankenGame/MixiViewController.h
@@ -10,4 +10,20 @@
 
 @interface MixiViewController : UIViewController
 
+
+@property (weak, nonatomic) IBOutlet UILabel *label_title;
+// @property (weak, nonatomic) IBOutlet UILabel *label_taisen_aite;
+@property (weak, nonatomic) IBOutlet UILabel *label_kekka;
+@property (weak, nonatomic) IBOutlet UIButton *btn_pa_outlet;
+@property (weak, nonatomic) IBOutlet UIButton *btn_choki_outlet;
+@property (weak, nonatomic) IBOutlet UIButton *btn_gu_outlet;
+@property (weak, nonatomic) IBOutlet UIButton *btn_retry_outlet;
+@property (weak, nonatomic) IBOutlet UIImageView *aite_image;
+- (IBAction)btn_pa:(id)sender;
+- (IBAction)btn_gu:(id)sender;
+- (IBAction)btn_choki:(id)sender;
+- (IBAction)btn_retry:(id)sender;
+
+
+
 @end
--- jankenGame/MixiViewController.m
+++ jankenGame/MixiViewController.m
@@ -14,10 +14,24 @@
 
 @implementation MixiViewController
 
+UIImage *gu_img;
+UIImage *ch_img;
+UIImage *pa_img;
+
 - (void)viewDidLoad
 {
+
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.btn_retry_outlet.hidden = YES;
+    self.label_kekka.text = @"";
+    self.label_kekka.font = [UIFont boldSystemFontOfSize:30];
+    self.label_kekka.textColor = [[UIColor alloc] initWithRed:0.0 green:0.0 blue:0.0 alpha:1.0];
+    self.aite_image.image = nil;
+    
+    gu_img = [UIImage imageNamed:@"gu.png"];
+    ch_img = [UIImage imageNamed:@"ch.png"];
+    pa_img = [UIImage imageNamed:@"pa.png"];
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +40,124 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)btn_pa:(id)sender {
+    self.label_title.text = @"じゃんけん・・・ぽんっ！";
+    
+    srand(time(Nil));
+    NSInteger kekka_rand;
+    kekka_rand = rand() % 3;
+    if (kekka_rand == 0) {
+        self.aite_image.image = pa_img;
+        self.label_kekka.text = @"あいこで・・・";
+        
+    } else if (kekka_rand == 1) {
+        self.aite_image.image = gu_img;
+        self.label_kekka.text = @"あなたのかちです！";
+        self.label_kekka.textColor = [[UIColor alloc] initWithRed:0.0 green:0.0 blue:1.0 alpha:1.0];
+        
+        self.btn_gu_outlet.hidden = YES;
+        self.btn_choki_outlet.hidden = YES;
+        self.btn_pa_outlet.enabled = NO;
+        self.btn_retry_outlet.hidden = NO;
+        
+    } else {
+        self.aite_image.image = ch_img;
+        self.label_kekka.text = @"あなたのまけです";
+        self.label_kekka.textColor = [[UIColor alloc] initWithRed:1.0 green:0.0 blue:0.0 alpha:1.0];
+        
+        self.btn_gu_outlet.hidden = YES;
+        self.btn_choki_outlet.hidden = YES;
+        self.btn_pa_outlet.enabled = NO;
+        self.btn_retry_outlet.hidden = NO;
+    }
+}
+
+- (IBAction)btn_gu:(id)sender {
+    self.label_title.text = @"じゃんけん・・・ぽんっ！";
+    
+    srand(time(Nil));
+    NSInteger kekka_rand;
+    kekka_rand = rand() % 3;
+    if (kekka_rand == 0) {
+        
+        self.aite_image.image = gu_img;
+        self.label_kekka.text = @"あいこで・・・";
+        
+    } else if (kekka_rand == 1) {
+        self.aite_image.image = ch_img;
+        self.label_kekka.text = @"あなたのかちです！";
+        self.label_kekka.textColor = [[UIColor alloc] initWithRed:0.0 green:0.0 blue:1.0 alpha:1.0];
+        
+        
+        self.btn_pa_outlet.hidden = YES;
+        self.btn_choki_outlet.hidden = YES;
+        self.btn_gu_outlet.enabled = NO;
+        self.btn_retry_outlet.hidden = NO;
+        
+    } else {
+        self.aite_image.image = pa_img;
+        self.label_kekka.text = @"あなたのまけです";
+        self.label_kekka.textColor = [[UIColor alloc] initWithRed:1.0 green:0.0 blue:0.0 alpha:1.0];
+        
+        self.btn_pa_outlet.hidden = YES;
+        self.btn_choki_outlet.hidden = YES;
+        self.btn_gu_outlet.enabled = NO;
+        self.btn_retry_outlet.hidden = NO;
+    }
+}
+
+- (IBAction)btn_choki:(id)sender {
+    self.label_title.text = @"じゃんけん・・・ぽんっ！";
+    
+    srand(time(Nil));
+    NSInteger kekka_rand;
+    kekka_rand = rand() % 3;
+    if (kekka_rand == 0) {
+        self.aite_image.image = ch_img;
+        self.label_kekka.text = @"あいこで・・・";
+        
+    } else if (kekka_rand == 1) {
+        self.aite_image.image = pa_img;
+        self.label_kekka.text = @"あなたのかちです！";
+        self.label_kekka.textColor = [[UIColor alloc] initWithRed:0.0 green:0.0 blue:1.0 alpha:1.0];
+        
+        
+        self.btn_gu_outlet.hidden = YES;
+        self.btn_pa_outlet.hidden = YES;
+        self.btn_choki_outlet.enabled = NO;
+        self.btn_retry_outlet.hidden = NO;
+        
+    } else {
+        self.aite_image.image = gu_img;
+        self.label_kekka.text = @"あなたのまけです";
+        self.label_kekka.textColor = [[UIColor alloc] initWithRed:1.0 green:0.0 blue:0.0 alpha:1.0];
+        
+        self.btn_gu_outlet.hidden = YES;
+        self.btn_pa_outlet.hidden = YES;
+        self.btn_choki_outlet.enabled = NO;
+        self.btn_retry_outlet.hidden = NO;
+    }
+
+}
+
+- (IBAction)btn_retry:(id)sender {
+    
+    self.label_title.text = @"じゃんけん・・・";
+    self.label_kekka.text = @"";
+    
+    self.aite_image.image = nil;
+    self.label_kekka.textColor = [[UIColor alloc] initWithRed:0.0 green:0.0 blue:0.0 alpha:1.0];
+    
+    
+    self.btn_pa_outlet.hidden = NO;
+    self.btn_choki_outlet.hidden = NO;
+    self.btn_gu_outlet.hidden = NO;
+    
+    self.btn_gu_outlet.enabled = YES;
+    self.btn_pa_outlet.enabled = YES;
+    self.btn_choki_outlet.enabled = YES;
+    
+    self.btn_retry_outlet.hidden = YES;
+    self.aite_image.image = nil;
+}
 @end
